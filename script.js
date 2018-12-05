ffa781f13884c1d7a001591f0ee4b0965aa95e0f
davis opondo <opondodav201994@gmail.com>
2018-12-05 10:59:27 +0300

    add js function

---
+++ script.js
@@ -0,0 +1,7 @@
+function triangle() {
+    
+    var sides = [];
+
+    sides.push(document.getElementById("num1").value);
+    sides.push(document.getElementById("num2").value);
+    sides.push(document.getElementById("num3").value);
